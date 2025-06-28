2c4c5d22bcecb89c138219f1a07515a85a08b839
Royn <bluecraft169@gmail.com>
2025-06-28 17:01:04 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/globals.css 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,42 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { checkInput, sendQuery } from "../components/InputValidator";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const router = useRouter()
+  const [tempSwitch, setTemp] = React.useState("Celcius");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const setC = () => setTemp("Celcius");
+  const setF = () => setTemp("Fahrenheit");
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-[#898AC4]">
+      <div className="p-4 bg-[#A2AADB] rounded-2xl">
+        <div className="mt-3 text-center text-[#FFF2E0]">
+          <p className="font-bold text-2xl">WeCheck</p>
+          <p className="font-semibold text-md">Simply Weather Checker</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="mt-3 relative w-full">
+          <input id="lokasion" className="w-full bg-[#FFF2E0] text-[#898AC4] rounded-3xl p-3 pr-13.5 outline-[#898AC4] focus:outline-4 transition-discrete duration-100" type="text" placeholder="Location/Coordinates.."
+          onChange={checkInput}/>
+          <button id="search-button" className="absolute right-3 top-1/2 -translate-y-1/2 bg-[#898AC4] text-[#FFF2E0] rounded-2xl p-1 hover:bg-[#C0C9EE] hover:text-[#898AC4] hover:outline-2 active:scale-85 outline-[#898AC4] transition-discrete duration-75 cursor-pointer" onClick={() => sendQuery(router)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0s.41-1.08 0-1.49zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"/></svg>
+          </button>
+        </div>
+
+        { /* <div className="mt-3 flex items-center justify-center">  Switcher }
+          <div className="bg-[#FFF2E0] rounded-2xl relative">
+            <span className={`absolute z-0 h-full bg-[#899AC4] rounded-2xl transition-all duration-300 ease-in-out ${tempSwitch === "Celcius" ? "left-0 w-24" : "left-24 w-33"}`} />
+            <button className={`relative z-10 px-6 font-medium rounded-2xl transition-colors duration-200 ease-in-out hover:cursor-pointer ${tempSwitch === "Celcius" ? "text-[#FFF2E0]" : "text-[#899AC4]"}`} onClick={setC}>Celcius</button>
+            <button className={`relative z-10 text-center px-6 font-medium rounded-2xl transition-colors duration-200 ease-in-out hover:cursor-pointer ${tempSwitch === "Fahrenheit" ? "text-[#FFF2E0]" : "text-[#899AC4]"}`} onClick={setF}>Fahrenheit</button> {/*${tempSwitch === "Fahrenheit" ? "bg-[#899AC4] text-[#FFF2E0]" : "text-[#899AC4]"}
+          </div>
+        </div> */}
+        
+        <footer className="mt-3 text-center text-sm font-light">Created By <Link className="font-bold" href="https://github.com/AdamRoyn">AdamRoyn</Link> (first project, yay!)</footer>
+      </div>
     </div>
   );
 }
